b4ea8abfcd6da61a5b379cc3934b094fc46366cb
Josh Cunningham <josh@joshcanhelp.com>
2022-08-05 11:21:07 -0700

    fix: startup script

--- startup.sh
+++ startup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir _solutions
-
 git clone git@github.com:joshcanhelp/oidc-tools _oidc-tools
 git clone git@github.com:joshcanhelp/express-utility-routes _express-utility-routes
 git clone git@github.com:joshcanhelp/authorization-server
