732c24805cfd14f5eed0368c62e79847bbe97a01
Josh Cunningham <josh@joshcanhelp.com>
2022-08-09 10:54:08 -0700

    feat: improve project switcher

--- switch_project.sh
+++ switch_project.sh
@@ -5,22 +5,30 @@
 #
 
 if [ $1 == "1:1" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout milestone-1-solution
+  git -C authorization-server pull
   exit 0
 fi
 
 if [ $1 == "1:2" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout milestone-2-solution
+  git -C authorization-server pull
   exit 0
 fi
 
 if [ $1 == "1:3" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout milestone-3-solution
+  git -C authorization-server pull
   exit 0
 fi
 
 if [ $1 == "1:4" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout milestone-4-solution
+  git -C authorization-server pull
   exit 0
 fi
 
@@ -29,26 +37,42 @@ fi
 #
 
 if [ $1 == "2:1" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-2-milestone-1-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-2-milestone-1-solution
+  git -C web-application pull
   exit 0
 fi
 
 if [ $1 == "2:2" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-2-milestone-1-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-2-milestone-2-solution
+  git -C web-application pull
   exit 0
 fi
 
 if [ $1 == "2:3" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-2-milestone-1-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-2-milestone-3-solution
+  git -C web-application pull
   exit 0
 fi
 
 if [ $1 == "2:4" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-2-milestone-1-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-2-milestone-4-solution
+  git -C web-application pull
   exit 0
 fi
 
@@ -57,26 +81,42 @@ fi
 #
 
 if [ $1 == "3:1" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-2-milestone-1-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-3-milestone-1-solution  
+  git -C web-application pull
   exit 0
 fi
 
 if [ $1 == "3:2" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-2-milestone-1-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-3-milestone-2-solution
+  git -C web-application pull
   exit 0
 fi
 
 if [ $1 == "3:3" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-3-milestone-3-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-3-milestone-3-solution
+  git -C web-application pull
   exit 0
 fi
 
 if [ $1 == "3:4" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-3-milestone-3-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-3-milestone-4-solution
+  git -C web-application pull
   exit 0
 fi
 
@@ -85,30 +125,54 @@ fi
 #
 
 if [ $1 == "4:1" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-4-milestone-1-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-4-milestone-1-solution
+  git -C web-application pull
+  echo "Single-page app:"
   git -C single-page-app checkout project-4-milestone-1-solution
+  git -C single-page-app pull
   exit 0
 fi
 
 if [ $1 == "4:2" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-4-milestone-1-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-4-milestone-1-solution
+  git -C web-application pull
+  echo "Single-page app:"
   git -C single-page-app checkout project-4-milestone-2-solution
+  git -C single-page-app pull
   exit 0
 fi
 
 if [ $1 == "4:3" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-4-milestone-1-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-4-milestone-1-solution
+  git -C web-application pull
+  echo "Single-page app:"
   git -C single-page-app checkout project-4-milestone-3-solution
+  git -C single-page-app pull
   exit 0
 fi
 
 if [ $1 == "4:4" ]; then
+  echo "Authorization server:"
   git -C authorization-server checkout project-4-milestone-1-solution
+  git -C authorization-server pull
+  echo "Web application:"
   git -C web-application checkout project-4-milestone-1-solution
+  git -C web-application pull
+  echo "Single-page app:"
   git -C single-page-app checkout project-4-milestone-4-solution
+  git -C single-page-app pull
   exit 0
 fi
 
