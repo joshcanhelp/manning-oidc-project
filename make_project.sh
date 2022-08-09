a5bd64da2d61a8c33fd6ce44bbdb30aee670e359
Josh Cunningham <josh@joshcanhelp.com>
2022-08-09 15:44:48 -0700

    feat: new sync scripts

--- make_project.sh
+++ make_project.sh
@@ -1,49 +1,49 @@
 #!/bin/bash
 
-rm ./_solutions/*.zip
+rm _solutions/*.zip
 
 ###
 ## Project 1
 #
 
 git -C authorization-server checkout milestone-1-solution
-zip -r _solutions/manning-oidc-project-1-milestone-1-solution.zip \ 
-  authorization-server \
+git -C authorization-server pull
+zip -r _solutions/manning-oidc-project-1-milestone-1-solution.zip authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout milestone-1-partial
-zip -r _solutions/manning-oidc-project-1-milestone-1-partial.zip \ 
-  authorization-server \
+git -C authorization-server pull
+zip -r _solutions/manning-oidc-project-1-milestone-1-partial.zip authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout milestone-2-solution
-zip -r _solutions/manning-oidc-project-1-milestone-2-solution.zip \ 
-  authorization-server \
+git -C authorization-server pull
+zip -r _solutions/manning-oidc-project-1-milestone-2-solution.zip authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout milestone-2-partial
-zip -r _solutions/manning-oidc-project-1-milestone-2-partial.zip \ 
-  authorization-server \
+git -C authorization-server pull
+zip -r _solutions/manning-oidc-project-1-milestone-2-partial.zip authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout milestone-3-solution
-zip -r _solutions/manning-oidc-project-1-milestone-3-solution.zip \ 
-  authorization-server \
+git -C authorization-server pull
+zip -r _solutions/manning-oidc-project-1-milestone-3-solution.zip authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout milestone-3-partial
-zip -r _solutions/manning-oidc-project-1-milestone-3-partial.zip \ 
-  authorization-server \
+git -C authorization-server pull
+zip -r _solutions/manning-oidc-project-1-milestone-3-partial.zip authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout milestone-4-solution
-zip -r _solutions/manning-oidc-project-1-milestone-4-solution.zip \ 
-  authorization-server \
+git -C authorization-server pull
+zip -r _solutions/manning-oidc-project-1-milestone-4-solution.zip authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout milestone-4-partial
-zip -r _solutions/manning-oidc-project-1-milestone-4-partial.zip \ 
-  authorization-server \
+git -C authorization-server pull
+zip -r _solutions/manning-oidc-project-1-milestone-4-partial.zip authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 ###
@@ -51,51 +51,59 @@ zip -r _solutions/manning-oidc-project-1-milestone-4-partial.zip \
 #
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-2-milestone-1-solution
-zip -r _solutions/manning-oidc-project-2-milestone-1-solution.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-2-milestone-1-solution.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-2-milestone-1-partial
-zip -r _solutions/manning-oidc-project-2-milestone-1-partial.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-2-milestone-1-partial.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-2-milestone-2-solution
-zip -r _solutions/manning-oidc-project-2-milestone-2-solution.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-2-milestone-2-solution.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-2-milestone-2-partial
-zip -r _solutions/manning-oidc-project-2-milestone-2-partial.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-2-milestone-2-partial.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-2-milestone-3-solution
-zip -r _solutions/manning-oidc-project-2-milestone-3-solution.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-2-milestone-3-solution.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-2-milestone-3-partial
-zip -r _solutions/manning-oidc-project-2-milestone-3-partial.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-2-milestone-3-partial.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-2-milestone-4-solution
-zip -r _solutions/manning-oidc-project-2-milestone-4-solution.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-2-milestone-4-solution.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-2-milestone-4-partial
-zip -r _solutions/manning-oidc-project-2-milestone-4-partial.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-2-milestone-4-partial.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 ###
@@ -103,51 +111,59 @@ zip -r _solutions/manning-oidc-project-2-milestone-4-partial.zip \
 #
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-3-milestone-1-solution
-zip -r _solutions/manning-oidc-project-3-milestone-1-solution.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-3-milestone-1-solution.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-3-milestone-1-partial
-zip -r _solutions/manning-oidc-project-3-milestone-1-partial.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-3-milestone-1-partial.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-3-milestone-2-solution
-zip -r _solutions/manning-oidc-project-3-milestone-2-solution.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-3-milestone-2-solution.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-2-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-3-milestone-2-partial
-zip -r _solutions/manning-oidc-project-3-milestone-2-partial.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-3-milestone-2-partial.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-3-milestone-3-solution
+git -C authorization-server pull
 git -C web-application checkout project-3-milestone-3-solution
-zip -r _solutions/manning-oidc-project-3-milestone-3-solution.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-3-milestone-3-solution.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-3-milestone-3-partial
+git -C authorization-server pull
 git -C web-application checkout project-3-milestone-3-partial
-zip -r _solutions/manning-oidc-project-3-milestone-3-partial.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-3-milestone-3-partial.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-3-milestone-3-solution
+git -C authorization-server pull
 git -C web-application checkout project-3-milestone-4-solution
-zip -r _solutions/manning-oidc-project-3-milestone-4-solution.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-3-milestone-4-solution.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-3-milestone-3-solution
+git -C authorization-server pull
 git -C web-application checkout project-3-milestone-4-partial
-zip -r _solutions/manning-oidc-project-3-milestone-4-partial.zip \ 
-  web-application authorization-server \
+git -C web-application pull
+zip -r _solutions/manning-oidc-project-3-milestone-4-partial.zip web-application authorization-server \
   -x "*/.git/*" -x "*/node_modules/*"
 
 ###
@@ -155,58 +171,74 @@ zip -r _solutions/manning-oidc-project-3-milestone-4-partial.zip \
 #
 
 git -C authorization-server checkout project-4-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-4-milestone-1-solution
+git -C web-application pull
 git -C single-page-app checkout project-4-milestone-1-solution
-zip -r _solutions/manning-oidc-project-4-milestone-1-solution.zip \
-  web-application authorization-server single-page-app \
+git -C single-page-app pull
+zip -r _solutions/manning-oidc-project-4-milestone-1-solution.zip web-application authorization-server single-page-app \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-4-milestone-1-partial
+git -C authorization-server pull
 git -C web-application checkout project-4-milestone-1-partial
+git -C web-application pull
 git -C single-page-app checkout project-4-milestone-1-partial
-zip -r _solutions/manning-oidc-project-4-milestone-1-partial.zip \
-  web-application authorization-server single-page-app \
+git -C single-page-app pull
+zip -r _solutions/manning-oidc-project-4-milestone-1-partial.zip web-application authorization-server single-page-app \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-4-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-4-milestone-1-solution
+git -C web-application pull
 git -C single-page-app checkout project-4-milestone-2-solution
-zip -r _solutions/manning-oidc-project-4-milestone-2-solution.zip \
-  web-application authorization-server single-page-app \
+git -C single-page-app pull
+zip -r _solutions/manning-oidc-project-4-milestone-2-solution.zip web-application authorization-server single-page-app \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-4-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-4-milestone-1-solution
+git -C web-application pull
 git -C single-page-app checkout project-4-milestone-2-partial
-zip -r _solutions/manning-oidc-project-4-milestone-2-partial.zip \
-  web-application authorization-server single-page-app \
+git -C single-page-app pull
+zip -r _solutions/manning-oidc-project-4-milestone-2-partial.zip web-application authorization-server single-page-app \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-4-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-4-milestone-1-solution
+git -C web-application pull
 git -C single-page-app checkout project-4-milestone-3-solution
-zip -r _solutions/manning-oidc-project-4-milestone-3-solution.zip \
-  web-application authorization-server single-page-app \
+git -C single-page-app pull
+zip -r _solutions/manning-oidc-project-4-milestone-3-solution.zip web-application authorization-server single-page-app \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-4-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-4-milestone-1-solution
+git -C web-application pull
 git -C single-page-app checkout project-4-milestone-3-partial
-zip -r _solutions/manning-oidc-project-4-milestone-3-partial.zip \
-  web-application authorization-server single-page-app \
+git -C single-page-app pull
+zip -r _solutions/manning-oidc-project-4-milestone-3-partial.zip web-application authorization-server single-page-app \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-4-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-4-milestone-1-solution
+git -C web-application pull
 git -C single-page-app checkout project-4-milestone-4-solution
-zip -r _solutions/manning-oidc-project-4-milestone-4-solution.zip \
-  web-application authorization-server single-page-app \
+git -C single-page-app pull
+zip -r _solutions/manning-oidc-project-4-milestone-4-solution.zip web-application authorization-server single-page-app \
   -x "*/.git/*" -x "*/node_modules/*"
 
 git -C authorization-server checkout project-4-milestone-1-solution
+git -C authorization-server pull
 git -C web-application checkout project-4-milestone-1-solution
+git -C web-application pull
 git -C single-page-app checkout project-4-milestone-4-partial
-zip -r _solutions/manning-oidc-project-4-milestone-4-partial.zip \
-  web-application authorization-server single-page-app \
+git -C single-page-app pull
+zip -r _solutions/manning-oidc-project-4-milestone-4-partial.zip web-application authorization-server single-page-app \
   -x "*/.git/*" -x "*/node_modules/*"
   
---
+++ search_all_code.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+###
+## Project 1
+#
+
+git -C authorization-server checkout milestone-1-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C authorization-server checkout milestone-1-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C authorization-server checkout milestone-2-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C authorization-server checkout milestone-2-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C authorization-server checkout milestone-3-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C authorization-server checkout milestone-3-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C authorization-server checkout milestone-4-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C authorization-server checkout milestone-4-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+
+
+###
+## Project 2
+#
+
+git -C authorization-server checkout project-2-milestone-1-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C web-application checkout project-2-milestone-1-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-2-milestone-1-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-2-milestone-2-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-2-milestone-2-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-2-milestone-3-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-2-milestone-3-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-2-milestone-4-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-2-milestone-4-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+
+
+###
+## Project 3
+#
+
+git -C web-application checkout project-3-milestone-1-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-3-milestone-1-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-3-milestone-2-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-3-milestone-2-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C authorization-server checkout project-3-milestone-3-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C web-application checkout project-3-milestone-3-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C authorization-server checkout project-3-milestone-3-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C web-application checkout project-3-milestone-3-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-3-milestone-4-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C web-application checkout project-3-milestone-4-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+
+
+###
+## Project 4
+#
+
+git -C authorization-server checkout project-4-milestone-1-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C web-application checkout project-4-milestone-1-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C single-page-app checkout project-4-milestone-1-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C authorization-server checkout project-4-milestone-1-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" authorization-server
+
+git -C web-application checkout project-4-milestone-1-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" web-application
+
+git -C single-page-app checkout project-4-milestone-1-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" single-page-app
+
+git -C single-page-app checkout project-4-milestone-2-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" single-page-app
+
+git -C single-page-app checkout project-4-milestone-2-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" single-page-app
+
+git -C single-page-app checkout project-4-milestone-3-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" single-page-app
+
+git -C single-page-app checkout project-4-milestone-3-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" single-page-app
+
+git -C single-page-app checkout project-4-milestone-4-solution
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" single-page-app
+
+git -C single-page-app checkout project-4-milestone-4-partial
+grep -r --exclude-dir=node_modules --exclude-dir=.git --exclude=package-lock.json "quick-lru" single-page-app
+
