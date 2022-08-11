bc7d29e2d9b1c1034fa47a53db599d5e7a48d5e3
Josh Cunningham <josh@joshcanhelp.com>
2022-08-11 10:29:58 -0700

    fix: mkdir for starter repos

--- startup.sh
+++ startup.sh
@@ -7,6 +7,8 @@ git clone git@github.com:joshcanhelp/manning-oidc-single-page-app.git single-pag
 git clone git@github.com:joshcanhelp/oidc-tools _oidc-tools
 git clone git@github.com:joshcanhelp/express-utility-routes _express-utility-routes
 
+mkdir _manning-starter-repos
+
 git clone git@github.com:manning-liveproject/login-with-OIDC-lp-author.git _manning-starter-repos/project-2
 git clone git@github.com:manning-liveproject/build-an-oauth2-protected-api-lp-author.git _manning-starter-repos/project-3
 git clone git@github.com:manning-liveproject/modify-a-spa-to-use-oidc-lp-author.git _manning-starter-repos/project-4
